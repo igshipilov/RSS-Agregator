96b75119595e61ee671501e6a6175cc187ce2336
Igor <igor_shipilov@inbox.ru>
2024-02-14 12:17:51 +0700

    update package.json, return orig webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,27 @@
-import HtmlWebpackPlugin from 'html-webpack-plugin';
+// Generated using webpack-cli https://github.com/webpack/webpack-cli
 
-export default {
-  mode: process.env.NODE_ENV || 'development',
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const isProduction = process.env.NODE_ENV == 'production';
+
+const config = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    open: true,
+    host: 'localhost',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+    }),
+
+    // Add your plugins here
+    // Learn more about plugins from https://webpack.js.org/configuration/plugins/
+  ],
   module: {
     rules: [
       {
@@ -14,11 +34,11 @@ export default {
           },
         },
       },
-      { test: /\.css$/, use: ['style-loader', 'css-loader', 'postcss-loader'] },
       {
         test: /\.scss$/,
         use: ['style-loader', 'css-loader', 'sass-loader', 'postcss-loader'],
       },
+      { test: /\.css$/, use: ['style-loader', 'css-loader', 'postcss-loader'] },
       {
         test: /\.woff2?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
         use: 'url-loader?limit=10000',
@@ -27,14 +47,19 @@ export default {
         test: /\.(ttf|eot|svg)(\?[\s\S]+)?$/,
         use: 'file-loader',
       },
+
+      // Add your rules for custom modules here
+      // Learn more about loaders from https://webpack.js.org/loaders/
     ],
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: 'template.html',
-    }),
-  ],
-  output: {
-    clean: true,
-  },
+};
+
+module.exports = () => {
+  if (isProduction) {
+    config.mode = 'production';
+  } else {
+    config.mode = 'development';
+  }
+
+  return config;
 };
