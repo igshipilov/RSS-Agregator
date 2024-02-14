cc7fc112eb0256dfade0059bb0f2e9158c82fc56
Igor <igor_shipilov@inbox.ru>
2024-02-14 15:01:45 +0700

    rollback to init.js v1

--- src/init.js
+++ src/init.js
@@ -1,24 +1,72 @@
-const express = require('express');
-const path = require('path');
+// ------- v1 ---------
+import Example from './Example.js';
 
-const app = express();
+export default () => {
+  const element = document.getElementById('point');
+  const obj = new Example(element);
+  obj.init();
+};
+// --------------------
 
-app.use(express.static(path.join(__dirname, 'dist')));
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
-});
 
-const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
 
-// import Example from './Example.js';
+// ------- v4 ---------
+// import { fileURLToPath } from 'url';
+// import { dirname } from 'path';
+// import express from 'express';
+// import path from 'path';
 
-// export default () => {
-//   const element = document.getElementById('point');
-//   const obj = new Example(element);
-//   obj.init();
-// };
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = dirname(__filename);
+
+
+// const app = express()
+// const port = process.env.PORT || 4000;
+
+// app.get('/', (req, res) => {
+//   res.send('Hello World!')
+// })
+
+// app.listen(port, () => {
+//   console.log(`Example app listening on port ${port}`)
+// })
+// --------------------
+
+
+
+// ------- v3 ---------
+// const express = require('express');
+
+// const app = express();
+// const port = process.env.PORT || 4000;
+
+// app.get('/', (req, res) => {
+//   res.send('Hello World!');
+// });
+
+// app.listen(port, () => {
+//   console.log(`Example app listening on port ${port}`);
+// });
+// --------------------
+
+
+// ------- v2 ---------
+// const express = require('express');
+// const path = require('path');
+
+// const app = express();
+
+// app.use(express.static(path.join(__dirname, 'dist')));
+
+// app.get('/', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+// });
+
+// const port = process.env.PORT || 3000;
+
+// app.listen(port, () => {
+//   console.log(`Server running on port ${port}`);
+// });
+// --------------------
