fd4fe397e5503c3ec4e1e46de0c2d707a1d65f7c
Igor <igor_shipilov@inbox.ru>
2024-02-22 15:59:08 +0700

    add feature: each 5-sec posts checks and render new posts only

--- bin/render.js
+++ bin/render.js
@@ -7,23 +7,125 @@ array-callback-return,
 no-unused-vars,
 
 */
+const handleInit = (titleName) => {
+  const container = document.createElement('div');
+  const containerForTitle = document.createElement('div');
+  const title = document.createElement('h2');
+  const list = document.createElement('ul');
+
+  container.classList.add('card', 'border-0');
+  containerForTitle.classList.add('card-body');
+  title.classList.add('card-title', 'h4');
+  list.classList.add('list-group', 'border-0', 'rounded-0');
+
+  title.textContent = titleName;
+
+  containerForTitle.append(title);
+  container.append(containerForTitle, list);
+
+  return container;
+};
+
+const handleFeedback = (elements, initialState, value, i18nInstance) => {
+  if (value === 'RSS успешно загружен') {
+    elements.input.classList.remove('is-invalid');
+    elements.input.value = '';
+    elements.input.focus();
+    elements.textFeedback.classList.remove('text-danger');
+    elements.textFeedback.classList.add('text-success');
+    elements.textFeedback.textContent = i18nInstance.t(initialState.form.feedback);
+  } else {
+    elements.input.classList.add('is-invalid');
+    elements.textFeedback.classList.remove('text-success');
+    elements.textFeedback.classList.add('text-danger');
+    elements.textFeedback.textContent = i18nInstance.t(initialState.form.feedback);
+  }
+};
+
+const handleFeeds = (value) => {
+  const list = document.querySelector('.feeds > .card > ul');
+  list.textContent = '';
+
+  value.map((feed) => {
+    const item = document.createElement('li');
+    const title = document.createElement('h3');
+    const description = document.createElement('p');
+
+    item.classList.add('list-group-item', 'border-0', 'border-end-0');
+    title.classList.add('h6', 'm-0');
+    description.classList.add('m-0', 'small', 'text-black-50');
+
+    title.textContent = feed.title;
+    description.textContent = feed.description;
+
+    item.append(title, description);
+    list.prepend(item);
+  });
+};
+
+const handlePosts = (elements, initialState, path, value, i18nInstance) => {
+  const list = document.querySelector('.posts > .card > ul');
+  // list.textContent = '';
+
+  value.map(({
+    title, link, description, id, feedId,
+  }) => {
+    const item = document.createElement('li');
+    const titleElement = document.createElement('a');
+    const button = document.createElement('button');
+
+    item.classList.add(
+      'list-group-item',
+      'd-flex',
+      'justify-content-between',
+      'align-items-start',
+      'border-0',
+      'border-end-0',
+    );
+    titleElement.classList.add('fw-bold');
+    button.classList.add('btn', 'btn-outline-primary', 'btn-sm');
+
+    titleElement.setAttribute('href', `${link}`);
+    titleElement.setAttribute('data-id', '84');
+    titleElement.setAttribute('target', '_blank');
+    titleElement.setAttribute('rel', 'noopener noreferrer');
+
+    button.setAttribute('type', 'button');
+    button.setAttribute('data-id', '82');
+    button.setAttribute('data-bs-toggle', 'modal');
+    button.setAttribute('data-bs-target', '#modal');
+
+    titleElement.textContent = title;
+    button.textContent = i18nInstance.t('buttons.view');
+
+    item.append(titleElement, button);
+    list.prepend(item);
+  });
+};
 
 export default (elements, initialState, i18nInstance) => (path, value) => {
-  // console.log(path)
+  console.log('RENDER >> path:', path, '>> value:', value); // debug
+
   switch (path) {
-    case 'form.error':
-      elements.input.classList.add('is-invalid');
-      elements.textFeedback.classList.remove('text-success');
-      elements.textFeedback.classList.add('text-danger');
-      elements.textFeedback.textContent = value;
+    case 'initiated':
+      elements.content.feeds.append(handleInit('Фиды'));
+      elements.content.posts.append(handleInit('Посты'));
+      break;
+
+    case 'form.feedback':
+      handleFeedback(elements, initialState, value, i18nInstance);
       break;
 
     case 'buttons.addDisabled':
-      if (value) {
-        elements.buttons.add.setAttribute('disabled', '');
-      } else {
-        elements.buttons.add.removeAttribute('disabled');
-      }
+      elements.buttons.add.toggleAttribute('disabled');
+      break;
+
+    case 'content.lists.feeds':
+      handleFeeds(value);
+      break;
+
+    case 'content.lists.newPosts':
+      handlePosts(elements, initialState, path, value, i18nInstance);
       break;
 
     default:
@@ -31,143 +133,6 @@ export default (elements, initialState, i18nInstance) => (path, value) => {
   }
 };
 
-// ----------------------------------------------------------------
-// ============== OLD render.js ===================================
-// ----------------------------------------------------------------
-
-// const handleFeedback = (elements, initialState, value, i18nInstance) => {
-//   if (value === false) {
-//     elements.input.classList.add('is-invalid');
-//     elements.textFeedback.classList.remove('text-success');
-//     elements.textFeedback.classList.add('text-danger');
-//     elements.textFeedback.textContent = i18nInstance.t(initialState.uiState.state);
-//   }
-//   if (value === true) {
-//     elements.input.classList.remove('is-invalid');
-//     elements.input.value = '';
-//     elements.input.focus();
-//     elements.textFeedback.classList.remove('text-danger');
-//     elements.textFeedback.classList.add('text-success');
-//     elements.textFeedback.textContent = i18nInstance.t(initialState.uiState.state);
-//   }
-// };
-
-// const handleInit = (titleName) => {
-//   const container = document.createElement('div');
-//   const containerForTitle = document.createElement('div');
-//   const title = document.createElement('h2');
-//   const list = document.createElement('ul');
-
-//   container.classList.add('card', 'border-0');
-//   containerForTitle.classList.add('card-body');
-//   title.classList.add('card-title', 'h4');
-//   list.classList.add('list-group', 'border-0', 'rounded-0');
-
-//   title.textContent = titleName;
-
-//   containerForTitle.append(title);
-//   container.append(containerForTitle, list);
-
-//   return container;
-// };
-
-// const handleContent = (elements, initialState, path, value, i18nInstance) => {
-//   if (path === 'content.feeds') {
-//     const list = document.querySelector('.feeds > .card > ul');
-//     list.textContent = '';
-
-//     value.map((feed) => {
-//       const item = document.createElement('li');
-//       const title = document.createElement('h3');
-//       const description = document.createElement('p');
-
-//       item.classList.add('list-group-item', 'border-0', 'border-end-0');
-//       title.classList.add('h6', 'm-0');
-//       description.classList.add('m-0', 'small', 'text-black-50');
-
-//       title.textContent = feed.title;
-//       description.textContent = feed.description;
-
-//       item.append(title, description);
-//       list.prepend(item);
-//     });
-//   }
-
-//   if (path === 'content.posts') {
-//     const list = document.querySelector('.posts > .card > ul');
-//     list.textContent = '';
-
-//     value.map(({
-//       title, link, description, id, feedId,
-//     }) => {
-//       const item = document.createElement('li');
-//       const titleElement = document.createElement('a');
-//       const button = document.createElement('button');
-
-//       item.classList.add(
-//         'list-group-item',
-//         'd-flex',
-//         'justify-content-between',
-//         'align-items-start',
-//         'border-0',
-//         'border-end-0',
-//       );
-//       titleElement.classList.add('fw-bold');
-//       button.classList.add('btn', 'btn-outline-primary', 'btn-sm');
-
-//       titleElement.setAttribute('href', `${link}`);
-//       titleElement.setAttribute('data-id', '84');
-//       titleElement.setAttribute('target', '_blank');
-//       titleElement.setAttribute('rel', 'noopener noreferrer');
-
-//       button.setAttribute('type', 'button');
-//       button.setAttribute('data-id', '82');
-//       button.setAttribute('data-bs-toggle', 'modal');
-//       button.setAttribute('data-bs-target', '#modal');
-
-//       titleElement.textContent = title;
-//       button.textContent = i18nInstance.t('buttons.view');
-
-//       item.append(titleElement, button);
-//       list.prepend(item);
-//     });
-//   }
-// };
-
-// export default (elements, initialState, i18nInstance) => (path, value) => {
-//   // console.log('RENDER >> path:', path, '>> value:', value); // debug
-//   switch (path) {
-//     case 'uiState.isValid':
-//       handleFeedback(elements, initialState, value, i18nInstance);
-//       break;
-
-//     case 'content.feeds':
-//       if (value) {
-//         const postsChildNodes = document.querySelector('.posts').childNodes;
-//         const isPostsInitiated = !!postsChildNodes.length;
-
-//         if (!isPostsInitiated) {
-//           elements.content.feeds.append(handleInit('Фиды'));
-//           elements.content.posts.append(handleInit('Посты'));
-//         }
-//       }
-//       handleContent(elements, initialState, path, value);
-//       break;
-
-//     case 'content.posts':
-//       if (value) {
-//         const postsChildNodes = document.querySelector('.posts').childNodes;
-//         const isPostsInitiated = !!postsChildNodes.length;
-
-//         if (!isPostsInitiated) {
-//           elements.content.feeds.append(handleInit('Фиды'));
-//           elements.content.posts.append(handleInit('Посты'));
-//         }
-//       }
-//       handleContent(elements, initialState, path, value, i18nInstance);
-//       break;
-
-//     default:
-//       throw new Error(`Unknown path: ${path}`);
-//   }
-// };
+// console.log('>>>>>>>>>> POSTS <<<<<<')
+// console.log(value)
+// console.log('>>>>>>>>>><<<<<<')
--- src/init.js
+++ src/init.js
@@ -5,7 +5,8 @@ no-console,
 func-names,
 consistent-return,
 no-shadow,
-max-len
+max-len,
+no-use-before-define,
 
 */
 
@@ -45,6 +46,7 @@ export default () => {
 
   const initialState = {
     subscribed: false,
+    timerOn: false,
     initiated: false,
     content: {
       modal: [], // [{ modalId: 1, visibility: true }, { modalId: 2, visibility: false }, ...]
@@ -56,8 +58,7 @@ export default () => {
       },
     },
     form: {
-      state: null, // 'error', 'success'
-      error: null, // 'invalidUrl', 'alreadyExists', 'parseError'
+      feedback: null,
     },
     buttons: {
       addDisabled: false,
@@ -89,7 +90,7 @@ export default () => {
 
   setLocale({
     string: {
-      url: () => ({ key: 'feedback.invalidUrl' }),
+      url: { key: 'feedback.invalidUrl' },
     },
   });
 
@@ -102,29 +103,8 @@ export default () => {
     })
     .required();
 
-  // -----------------------------------------------------------------------------
-  // Если это сабмит от юзера, то:
-  // рендер всех постов
-
-  // Если это не сабмит от юзера, то:
-  // добавляет только новые посты в state.newPosts,
-  // рендерит и затем удаляет их из state.newPosts
-
-  // То есть отслеживание обновлений рендерит только новые посты из уже добавленных урлов
-  // (поэтому feedId новых постов надо как-то связать с уже добавленными фидами)
-
-  // isSubmitted === bool
-  // он нужен, чтобы выбрать:
-  //  - или рендерить фиды и посты
-  //  - или рендерить только НОВЫЕ посты
-
-  // Ещё быть может возможно сделать эту функцию универсальной в части триггера рендера постов.
-  // Надо ещё подумать.
-
-  // ❌ FIXME -- кажется, не отрабатывает проверка новых поство и запись их в state
   const addFeedsAndPostsToState = (collFeedsAndPosts, isSubmitted) => {
     const { posts, feed } = collFeedsAndPosts;
-    // console.log(posts, feed); // debug
 
     const currentPosts = initialState.content.lists.posts;
     const newPosts = _.differenceWith(posts, currentPosts, _.isEqual);
@@ -138,11 +118,7 @@ export default () => {
       state.content.lists.newPosts.push(...newPosts);
       initialState.content.lists.newPosts = [];
     }
-
-    console.log(newPosts);
-    // console.log(initialState.content.lists)
   };
-    // -----------------------------------------------------------------------------
 
   const parseXML = (xml) => {
     const parser = new DOMParser();
@@ -155,23 +131,22 @@ export default () => {
       const feed = {
         title,
         description,
-
       };
 
       const items = parsed.documentElement.getElementsByTagName('item');
 
-      const posts = [...items].map((item) => {
+      const postsInit = [...items].map((item) => {
         const title = item.querySelector('title').textContent;
         const link = item.querySelector('link').textContent;
         const description = item.querySelector('description').textContent;
 
         return { title, link, description };
       });
+      const posts = postsInit.reverse();
 
       return { feed, posts };
     } catch (error) {
       initialState.uiState.state = 'feedback.parseError';
-      state.uiState.isValid = false;
     }
   };
 
@@ -196,12 +171,35 @@ export default () => {
     return (resultColl);
   };
 
-  const handleUrl = (url, e) => {
+  const getXML = (response, url) => {
+    if (response) {
+      const wasUrlAdded = initialState.content.lists.urls.includes(url);
+      if (!wasUrlAdded) {
+        initialState.content.lists.urls.push(url);
+      }
+      state.initiated = true; // триггерим первичный рендер (заголовки "Фиды" и "Посты", <ul>)
+
+      return response.data.contents; // xml → typeof: string
+    }
+    throw new Error('feedback.networkError');
+  };
+
+  const runTimer = () => {
+    if (initialState.subscribed === true && initialState.timerOn === false) {
+      setTimeout(function run() {
+        initialState.content.lists.urls.forEach((url) => handleUrl(url));
+        initialState.timerOn = true;
+        setTimeout(run, 5000);
+      }, 5000);
+    }
+  };
+
+  function handleUrl(url, e) {
     const isSubmitted = () => {
       try { return !!e; } catch (err) { return false; }
     };
 
-    // кнопка заблочится только если это сабмит (и не заблочится при автопроверке обновлений)
+    // block "Add" button on 'submit' event only (not block it on autoupdates)
     if (isSubmitted()) {
       state.buttons.addDisabled = true;
     }
@@ -209,30 +207,21 @@ export default () => {
     const proxyDisabledCache = 'https://allorigins.hexlet.app/get?disableCache=true&url=';
 
     axios.get(`${proxyDisabledCache}${encodeURIComponent(`${url}`)}`)
-      .then((response) => {
-        if (response) {
-          const wasUrlAdded = initialState.content.lists.urls.includes(url);
-          if (!wasUrlAdded) {
-            initialState.content.lists.urls.push(url);
-          }
-          state.initiated = true; // триггерим первичный рендер (заголовки "Фиды" и "Посты", <ul>)
-
-          return response.data.contents; // xml → typeof: string
-        }
-        throw new Error('feedback.networkError');
-      })
+      .then((response) => getXML(response, url))
       .then((xml) => parseXML(xml))
       .then((coll) => addIDs(coll))
       .then((collWithIDs) => addFeedsAndPostsToState(collWithIDs, isSubmitted())) // здесь триггерим рендер
       .then(() => {
         state.buttons.addDisabled = false;
         initialState.subscribed = true;
+        runTimer();
       })
       .catch((err) => { // обработчик ошибки Сети
-        state.form.error = i18nInstance.t(err.message);
+        console.log(err);
+        state.form.feedback = i18nInstance.t(err.message);
         state.buttons.addDisabled = false;
       });
-  };
+  }
 
   elements.form.addEventListener('submit', (e) => {
     e.preventDefault();
@@ -243,125 +232,10 @@ export default () => {
     // в schema.validate второй аргумент { urls: ... } нужен для yup.test('not-one-of')
     schema.validate(submittedUrl, { urls: initialState.content.lists.urls })
       .then(() => handleUrl(submittedUrl, e))
+      .then(() => { state.form.feedback = i18nInstance.t('feedback.success'); })
       .catch((err) => { // обработчик ошибок ввода (юзер ошибся)
-        state.form.error = err.errors.map((curErr) => i18nInstance.t(curErr.key));
+        state.form.feedback = err.errors.map((curErr) => i18nInstance.t(curErr.key));
         state.buttons.addDisabled = false;
       });
   });
-
-  // ❌ FIXME -- кажется, не отрабатывает таймер
-  if (initialState.subscribed === true) {
-    setTimeout(function run() {
-      initialState.content.lists.urls.forEach((url) => handleUrl(url));
-      setTimeout(run, 1000);
-    }, 0);
-  }
 };
-
-// ----------------------------------------------------------------
-// ============== OLD init.js =====================================
-// ----------------------------------------------------------------
-//   initialRender(elements, i18nInstance);
-
-//   const parseXML = (xml) => {
-//     const parser = new DOMParser();
-
-//     try {
-//       const parsed = parser.parseFromString(xml, 'text/xml');
-
-//       const setId = (currentTitle, type) => {
-//         const wasFeedAdded = initialState.content[type].find(({ title }) => title === currentTitle);
-//         return wasFeedAdded ? wasFeedAdded.id : _.uniqueId()
-//       };
-
-//       const title = parsed.documentElement.getElementsByTagName('title')[0].textContent;
-//       const description = parsed.documentElement.getElementsByTagName('description')[0].textContent;
-
-//       const feed = {
-//         title,
-//         description,
-//         id: setId(title, 'feeds'),
-//       };
-
-//       const items = parsed.documentElement.getElementsByTagName('item');
-
-//       const posts = [...items].map((item) => {
-//         const title = item.querySelector('title').textContent;
-//         const link = item.querySelector('link').textContent;
-//         const description = item.querySelector('description').textContent;
-//         const id = setId(title, 'posts');
-//         const feedId = feed.id;
-
-//         return {
-//           title, link, description, id, feedId,
-//         };
-//       });
-
-//       return { feed, posts };
-
-//     } catch (error) {
-//       initialState.uiState.state = 'feedback.parseError';
-//       state.uiState.isValid = false;
-//     }
-//   };
-
-//   const addFeedsAndPostsToState = (xml) => {
-//     const parsedData = parseXML(xml);
-//     const currentPosts = initialState.content.posts;
-//     const parsedPosts = parsedData.posts;
-//     const newPosts = _.differenceWith(parsedPosts, currentPosts, _.isEqual);
-//     const hasNewPosts = !_.isEmpty(newPosts);
-
-//     if (hasNewPosts) {
-//       // state.content.feeds.push(parsedData.feed);
-//       state.content.posts.push(...newPosts);
-//     }
-//   };
-
-//   const getXmlFromUrl = (url) => axios.get(
-//     `https://allorigins.hexlet.app/get?disableCache=true&url=${encodeURIComponent(`${url}`)}`
-//     )
-//   .then((response) => {
-//     if (response) return response.data.contents;
-//     throw new Error('Network response was not ok.');
-//   });
-
-//   const addContentFromUrl = (submittedUrl) => {
-//     schema.validate(submittedUrl, { urls: initialState.urls })
-//       .then(() => {
-//         initialState.uiState.state = 'feedback.success';
-//         initialState.urls.push(submittedUrl);
-//         // сбрасываю статус на null, чтобы рендерился контент из идущего подряд валидного url
-//         initialState.uiState.isValid = null;
-
-//         state.uiState.isValid = true;
-//         getXmlFromUrl(submittedUrl).then((xml) => addFeedsAndPostsToState(xml));
-//       })
-//       .catch((error) => {
-//         initialState.uiState.state = error.errors.map((curErr) => i18nInstance.t(curErr.key));
-
-//         // сбрасываю статус на null, чтобы рендерилась ошибка, идущая подряд
-//         initialState.uiState.isValid = null;
-//         state.uiState.isValid = false;
-//       });
-//   };
-
-// const postIfPostsUpdated = () => {
-//   setTimeout(function run() {
-//     initialState.urls.forEach((url) => {
-//       getXmlFromUrl(url).then((xml) => addFeedsAndPostsToState(xml));
-//     });
-//     setTimeout(run, 1000);
-//   }, 0)
-// };
-
-//   elements.form.addEventListener('submit', (e) => {
-//     e.preventDefault();
-
-//     const formData = new FormData(e.target);
-//     const submittedUrl = formData.get('url');
-
-//     addContentFromUrl(submittedUrl);
-//     postIfPostsUpdated();
-//   });
-// };
