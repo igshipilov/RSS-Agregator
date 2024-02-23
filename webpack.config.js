2926a32d27f8d625adaf6ec9ee2e03a1e6bcf990
Igor <igor_shipilov@inbox.ru>
2024-02-23 21:22:44 +0700

    change completely package.json and webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,16 @@
-// Generated using webpack-cli https://github.com/webpack/webpack-cli
+import path from 'node:path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import { fileURLToPath } from 'url';
 
-// import path from 'node:path';
-// import HtmlWebpackPlugin from 'html-webpack-plugin';
-// import { fileURLToPath } from 'url';
+const isProduction = process.env.NODE_ENV === 'production';
 
-// const isProduction = process.env.NODE_ENV === 'production';
-
-// const filename = fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(filename);
-
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-
-const isProduction = process.env.NODE_ENV == 'production';
+const filename = fileURLToPath(import.meta.url);
+const dirname = path.dirname(filename);
 
 const config = {
   entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(dirname, 'dist'),
   },
   devServer: {
     open: true,
@@ -27,53 +20,30 @@ const config = {
     new HtmlWebpackPlugin({
       template: 'index.html',
     }),
-
-    // Add your plugins here
-    // Learn more about plugins from https://webpack.js.org/configuration/plugins/
   ],
-  // resolve: {
-  //   fallback: {
-  //     fs: require.resolve('browserify-fs'), // Use a polyfill for fs
-  //   },
-  // },
   module: {
     rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'],
-          },
-        },
-      },
+      { test: /\.css$/, use: ['style-loader', 'css-loader', 'postcss-loader'] },
       {
         test: /\.scss$/,
         use: ['style-loader', 'css-loader', 'sass-loader', 'postcss-loader'],
       },
-      { test: /\.css$/, use: ['style-loader', 'css-loader', 'postcss-loader'] },
       {
-        test: /\.woff2?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        use: 'url-loader?limit=10000',
+        test: /\.(js|jsx)$/i,
       },
       {
-        test: /\.(ttf|eot|svg)(\?[\s\S]+)?$/,
-        use: 'file-loader',
+        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+        type: 'asset',
       },
-
-      // Add your rules for custom modules here
-      // Learn more about loaders from https://webpack.js.org/loaders/
     ],
   },
 };
 
-module.exports = () => {
+export default () => {
   if (isProduction) {
     config.mode = 'production';
   } else {
     config.mode = 'development';
   }
-
   return config;
 };
