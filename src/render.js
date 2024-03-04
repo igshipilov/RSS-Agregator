1365c6379a3b70b7c95fc5eb742e4ce0e5a3e922
Igor <igor_shipilov@inbox.ru>
2024-03-04 16:15:00 +0700

    run ci

--- src/render.js
+++ src/render.js
@@ -1,13 +1,4 @@
-/* eslint-disable
-
-no-param-reassign,
-max-len,
-
-*/
-
-// const mappingFeedback = {
-//   success:
-// };
+/* eslint-disable no-param-reassign, */
 
 const addTitles = (titleName) => {
   const container = document.createElement('div');
@@ -29,9 +20,8 @@ const addTitles = (titleName) => {
   return container;
 };
 
-const addFeeds = (feeds) => {
+const renderFeeds = (feeds) => {
   const list = document.querySelector('.feeds > .card > ul');
-  // list.textContent = '';
 
   feeds.map((feed) => {
     const item = document.createElement('li');
@@ -50,7 +40,7 @@ const addFeeds = (feeds) => {
   });
 };
 
-const addPosts = (posts, initialState, i18nInstance) => {
+const renderPosts = (posts, initialState, i18nInstance) => {
   const list = document.querySelector('.posts > .card > ul');
 
   posts.map(({
@@ -69,13 +59,11 @@ const addPosts = (posts, initialState, i18nInstance) => {
       'border-end-0',
     );
 
-    // 
     if (initialState.ui.clickedPostsIds.includes(id)) {
       titleElement.classList.add('fw-normal', 'link-secondary');
     } else {
       titleElement.classList.add('fw-bold');
     }
-    // 
 
     button.classList.add('btn', 'btn-outline-primary', 'btn-sm');
 
@@ -97,11 +85,17 @@ const addPosts = (posts, initialState, i18nInstance) => {
   });
 };
 
-// TODO
-// const handleError = () => {};
+const renderMessageError = (initialState, elements, i18nInstance, path) => {
+  const currentProcess = path.replace('.status', '');
+  const errorCode = initialState[currentProcess].error;
+  elements.input.classList.add('is-invalid');
+  elements.textFeedback.classList.remove('text-success');
+  elements.textFeedback.classList.add('text-danger');
+  elements.textFeedback.textContent = i18nInstance.t(errorCode);
+};
 
-// Рендерит контент: заголовки, описание
-const handleProcessStatus = (elements, initialState, i18nInstance, value) => {
+// Render content
+const handleProcessStatus = (elements, initialState, i18nInstance, path, value) => {
   switch (value) {
     case 'starting':
       break;
@@ -112,49 +106,45 @@ const handleProcessStatus = (elements, initialState, i18nInstance, value) => {
       elements.content.feeds.append(addTitles(i18nInstance.t('mainInterface.feedsTitle')));
       elements.content.posts.append(addTitles(i18nInstance.t('mainInterface.postsTitle')));
 
-      addFeeds(initialState.content.feeds);
-      addPosts(initialState.content.posts, initialState, i18nInstance);
+      renderFeeds(initialState.content.feeds);
+      renderPosts(initialState.content.posts, initialState, i18nInstance);
       break;
 
-    // TODO
     case 'uploadError':
-      const errorCode = initialState.loadingProcess.error;
-      elements.input.classList.add('is-invalid');
-      elements.textFeedback.classList.remove('text-success');
-      elements.textFeedback.classList.add('text-danger');
-      elements.textFeedback.textContent = i18nInstance.t(errorCode);
+      renderMessageError(initialState, elements, i18nInstance, path);
+
       break;
 
     default:
-      break;
+      throw new Error(`Unknown value: ${value}`);
   }
 };
 
-// Рендерит сообщения об успехе (зел.) и об ошибках (красн.)
-const handleFormStatus = (elements, initialState, i18nInstance, value) => {
+const renderMessageSuccess = (elements, i18nInstance) => {
+  elements.input.classList.remove('is-invalid');
+  elements.form.reset();
+  elements.input.focus();
+  elements.textFeedback.classList.remove('text-danger');
+  elements.textFeedback.classList.add('text-success');
+  elements.textFeedback.textContent = i18nInstance.t('feedback.success');
+};
+
+// Render messages: success and errors
+const handleFormStatus = (elements, initialState, i18nInstance, path, value) => {
   switch (value) {
     case 'sending':
       break;
 
     case 'sent':
-      elements.input.classList.remove('is-invalid');
-      elements.form.reset();
-      elements.input.focus();
-      elements.textFeedback.classList.remove('text-danger');
-      elements.textFeedback.classList.add('text-success');
-      elements.textFeedback.textContent = i18nInstance.t('feedback.success');
+      renderMessageSuccess(elements, i18nInstance);
       break;
 
     case 'validationError':
-      const errorCode = initialState.form.error;
-      elements.input.classList.add('is-invalid');
-      elements.textFeedback.classList.remove('text-success');
-      elements.textFeedback.classList.add('text-danger');
-      elements.textFeedback.textContent = i18nInstance.t(errorCode);
+      renderMessageError(initialState, elements, i18nInstance, path);
       break;
 
     default:
-      break;
+      throw new Error(`Unknown value: ${value}`);
   }
 };
 
@@ -176,11 +166,11 @@ export default (elements, initialState, i18nInstance) => (path, value) => {
   // console.log(path);
   switch (path) {
     case 'loadingProcess.status':
-      handleProcessStatus(elements, initialState, i18nInstance, value);
+      handleProcessStatus(elements, initialState, i18nInstance, path, value);
       break;
 
     case 'form.status':
-      handleFormStatus(elements, initialState, i18nInstance, value);
+      handleFormStatus(elements, initialState, i18nInstance, path, value);
       break;
 
     case 'ui.activePostId':
@@ -188,165 +178,6 @@ export default (elements, initialState, i18nInstance) => (path, value) => {
       break;
 
     default:
-      break;
-    //   throw new Error(`Unknown path: ${path}`);
+      throw new Error(`Unknown path: ${path}`);
   }
 };
-
-// ======== OLD =============
-
-// TODO use this code for changeLanguge feature
-// const renderInitialInterface = (elements, i18nInstance) => {
-//   elements.mainInterface.title.textContent = i18nInstance.t('mainInterface.title');
-//   elements.mainInterface.subtitle.textContent = i18nInstance.t('mainInterface.subtitle');
-//   elements.mainInterface.title.textContent = i18nInstance.t('mainInterface.title');
-//   elements.mainInterface.formPlaceholder.textContent = i18nInstance.t('mainInterface.formPlaceholder');
-//   elements.buttons.add.textContent = i18nInstance.t('buttons.add');
-// };
-
-// const handleInit = (titleName) => {
-//   const container = document.createElement('div');
-//   const containerForTitle = document.createElement('div');
-//   const title = document.createElement('h2');
-//   const list = document.createElement('ul');
-
-//   container.classList.add('card', 'border-0');
-//   containerForTitle.classList.add('card-body');
-//   title.classList.add('card-title', 'h4');
-//   list.classList.add('list-group', 'border-0', 'rounded-0');
-
-//   title.textContent = titleName;
-
-//   containerForTitle.append(title);
-//   container.append(containerForTitle, list);
-
-//   return container;
-// };
-
-// const handleFeedback = (elements, initialState, i18nInstance) => {
-//   if (initialState.isFormValid) {
-//     elements.input.classList.remove('is-invalid');
-//     elements.form.reset();
-//     elements.input.focus();
-//     elements.textFeedback.classList.remove('text-danger');
-//     elements.textFeedback.classList.add('text-success');
-//     elements.textFeedback.textContent = i18nInstance.t(initialState.form.feedback);
-//   } else {
-//     elements.input.classList.add('is-invalid');
-//     elements.textFeedback.classList.remove('text-success');
-//     elements.textFeedback.classList.add('text-danger');
-//     elements.textFeedback.textContent = i18nInstance.t(initialState.form.feedback);
-//   }
-// };
-
-// const handleFeeds = (value) => {
-//   const list = document.querySelector('.feeds > .card > ul');
-//   list.textContent = '';
-
-//   value.map((feed) => {
-//     const item = document.createElement('li');
-//     const title = document.createElement('h3');
-//     const description = document.createElement('p');
-
-//     item.classList.add('list-group-item', 'border-0', 'border-end-0');
-//     title.classList.add('h6', 'm-0');
-//     description.classList.add('m-0', 'small', 'text-black-50');
-
-//     title.textContent = feed.title;
-//     description.textContent = feed.description;
-
-//     item.append(title, description);
-//     list.prepend(item);
-//   });
-// };
-
-// const handlePosts = (value, i18nInstance) => {
-//   const list = document.querySelector('.posts > .card > ul');
-
-//   value.map(({
-//     title, link, id,
-//   }) => {
-//     const item = document.createElement('li');
-//     const titleElement = document.createElement('a');
-//     const button = document.createElement('button');
-
-//     item.classList.add(
-//       'list-group-item',
-//       'd-flex',
-//       'justify-content-between',
-//       'align-items-start',
-//       'border-0',
-//       'border-end-0',
-//     );
-//     titleElement.classList.add('fw-bold');
-//     button.classList.add('btn', 'btn-outline-primary', 'btn-sm');
-
-//     titleElement.setAttribute('href', `${link}`);
-//     titleElement.setAttribute('data-id', `${id}`);
-//     titleElement.setAttribute('target', '_blank');
-//     titleElement.setAttribute('rel', 'noopener noreferrer');
-
-//     button.setAttribute('type', 'button');
-//     button.setAttribute('data-id', `${id}`);
-//     button.setAttribute('data-bs-toggle', 'modal');
-//     button.setAttribute('data-bs-target', '#modal');
-
-//     titleElement.textContent = title;
-//     button.textContent = i18nInstance.t('buttons.view');
-
-//     item.append(titleElement, button);
-//     list.prepend(item);
-//   });
-// };
-
-// const handlerClickedPost = (elements, initialState, value) => {
-//   const currentPost = document.querySelector(`[data-id="${value}"]`);
-//   const { posts } = initialState.content;
-//   const currentPostData = posts.find(({ id }) => id === value);
-//   const { title, description, link } = currentPostData;
-
-//   currentPost.classList.remove('fw-bold');
-//   currentPost.classList.add('fw-normal', 'link-secondary');
-
-//   elements.modal.title.textContent = title;
-//   elements.modal.body.textContent = description;
-//   elements.modal.fullArticle.setAttribute('href', `${link}`);
-// };
-
-// export default (elements, initialState, i18nInstance) => (path, value) => {
-//   switch (path) {
-//     case 'initiated':
-//       elements.content.feeds.append(handleInit(i18nInstance.t('mainInterface.feedsTitle')));
-//       elements.content.posts.append(handleInit(i18nInstance.t('mainInterface.postsTitle')));
-//       break;
-
-//     case 'form.feedback':
-//       handleFeedback(elements, initialState, i18nInstance);
-//       break;
-
-//     case 'buttons.addDisabled':
-//       elements.buttons.add.toggleAttribute('disabled');
-//       elements.input.toggleAttribute('readonly');
-//       break;
-
-//     case 'content.feeds':
-//       handleFeeds(value);
-//       break;
-
-//     case 'content.newPosts':
-//       handlePosts(value, i18nInstance);
-//       break;
-
-//     case 'ui.activePostId':
-//       handlerClickedPost(elements, initialState, value);
-//       break;
-
-//       // TODO use this code for changeLanguge feature
-//       // case 'firstRender':
-//       //   renderInitialInterface(elements, i18nInstance);
-//       //   break;
-
-//     default:
-//       throw new Error(`Unknown path: ${path}`);
-//   }
-// };
